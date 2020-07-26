1a983b4dd58dd28c727ed54b8ebdcd8b2553763b
fernandonavajas <fernando.navajaso@utem.cl>
2020-07-26 16:58:51 -0400

    Se agregaron servicios dummy para pruebas

--- project/api/__init__.py
+++ project/api/__init__.py
@@ -1,8 +1,14 @@
 from flask_restplus import Api
 
 from project.api.ping import ping_namespace
+from project.api.top10Carreras import top10_namespace
+from project.api.infoCarreras import infoCarreras_namespace
+from project.api.infoCarrera import infoCarrera_namespace
 
 
+api = Api(version="1.0", title="API REST", doc="/doc/")
 
-api = Api(version="1.0", title="Onboarding", doc="/doc/")
 api.add_namespace(ping_namespace, path="/ping")
+api.add_namespace(top10_namespace, path="/top10")
+api.add_namespace(infoCarreras_namespace, path="/infoCarreras")
+api.add_namespace(infoCarrera_namespace, path="/infoCarrera")
